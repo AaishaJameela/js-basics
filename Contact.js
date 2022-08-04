2f3c9354f8e78445348f4dee7f30df2e9cb10eca
AaishaJameela <109809658+AaishaJameela@users.noreply.github.com>
2022-08-04 10:50:15 +0530

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from'./components/Header';
+import Footer from './components/Footer';
+import Sidebar from'./components/Sidebar';
+
+const ContactUs = () => {
+    return (
+
+        <div>
+        <Header />
+        <Sidebar />
+        <h1>Contact US Page</h1>
+        <Footer />
+        </div>
+           );
+    }
+    export default ContactUs;
